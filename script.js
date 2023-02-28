e4c7bcd269f01e99a40eb3a5384539e0469f9bca
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-02-28 15:27:07 +0200

    Create script.js
    debounce

---
+++ script.js
@@ -0,0 +1,11 @@
+function debounce(func, delay) {
+  let timeoutId;
+  return function(...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+}
+
+const debouncedHandleMouseMove = debounce(handleMouseMove, 250);
